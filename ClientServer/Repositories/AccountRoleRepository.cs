b8d752bd351cea9a8f181a7d02bf77550e4e620d
ahmadeko2017 <44262212+ahmadeko2017@users.noreply.github.com>
2023-07-20 14:46:25 +0700

    Membuat API AccountRole

---
+++ ClientServer/Repositories/AccountRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClientServer.Contracts;
+using ClientServer.Data;
+using ClientServer.Models;
+
+namespace ClientServer.Repositories;
+
+public class AccountRoleRepository : GeneralRepository<AccountRole>, IAccountRoleRepository
+{
+    /// <inheritdoc />
+    public AccountRoleRepository(BookingDbContext context) : base(context)
+    {
+    }
+}
