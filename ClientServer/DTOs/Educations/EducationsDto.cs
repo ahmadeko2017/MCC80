176f4e3683b2d67339aca96a08f0879179cee8c2
ahmadeko2017 <44262212+ahmadeko2017@users.noreply.github.com>
2023-07-22 16:04:41 +0700

    Selesai membuat semua DTOs

---
+++ ClientServer/DTOs/Educations/EducationsDto.cs
@@ -0,0 +1,38 @@
+﻿using ClientServer.Models;
+
+namespace ClientServer.DTOs.Educations;
+
+public class EducationsDto
+{
+    public Guid Guid { get; set; }
+    public string Major { get; set; }
+    public string Degree { get; set; }
+    public float GPA { get; set; }
+    public Guid UniversityGuid { get; set; }
+
+    public static implicit operator Education(EducationsDto educationsDto)
+    {
+        return new Education()
+        {
+            Guid = educationsDto.Guid,
+            Major = educationsDto.Major,
+            Degree = educationsDto.Degree,
+            GPA = educationsDto.GPA,
+            UniversityGuid = educationsDto.Guid,
+            CreatedDate = DateTime.Now,
+            ModifiedDate = DateTime.Now
+        };
+    }
+
+    public static explicit operator EducationsDto(Education education)
+    {
+        return new EducationsDto()
+        {
+            Guid = education.Guid,
+            Major = education.Major,
+            Degree = education.Degree,
+            GPA = education.GPA,
+            UniversityGuid = education.UniversityGuid
+        };
+    }
+}
